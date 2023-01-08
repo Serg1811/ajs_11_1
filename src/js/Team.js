418e8eb39ae8a7d025b26d7599531ba744fc0cfc
Sergey Kazakov <serg181182@mail.ru>
2023-01-08 20:36:27 +0600

    first commit

---
+++ src/js/Team.js
@@ -0,0 +1,19 @@
+export default class Team {
+  constructor(...players) {
+    this.players = players;
+  }
+
+  [Symbol.iterator]() {
+    this.nextId = 0;
+    this.current = this.players[this.nextId];
+    return this;
+  }
+
+  next() {
+    if (this.nextId < this.players.length) {
+      const result = { done: false, value: this.players[this.nextId++] };
+      return result;
+    }
+    return { done: true };
+  }
+}
